7ac03d6059cb0d1c6538c99eb0eb653e4536d883
moisestech <m@moises.tech>
2022-11-13 21:52:47 -0500

    copyright

---
+++ components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './Footer.module.css'
+
+export default function Footer() {
+  return (
+    <>
+      <footer className={styles.footer}>
+        Copyright 2022
+        {/* <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you */}
+      </footer>
+    </>
+  )
+}
